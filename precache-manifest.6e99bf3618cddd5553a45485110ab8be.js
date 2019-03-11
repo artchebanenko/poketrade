b650debf377c4d6a5bf48d58ef55ff16636ca535
Art <Art>
2019-03-11 13:56:09 +0200

    Ready Steady Go!

---
+++ precache-manifest.6e99bf3618cddd5553a45485110ab8be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "30bc8a16a6a63e980a4e06c11a66638b",
+    "url": "/poketrade/static/media/hero.30bc8a16.png"
+  },
+  {
+    "revision": "ce3a314ecd71025dc230e58c624f63f2",
+    "url": "/poketrade/static/media/card-back-small.ce3a314e.png"
+  },
+  {
+    "revision": "389bea3ad26498d2fd5a",
+    "url": "/poketrade/static/js/runtime~main.fc447214.js"
+  },
+  {
+    "revision": "73fdd076866b56b6d03a",
+    "url": "/poketrade/static/js/main.f2dbe5ee.chunk.js"
+  },
+  {
+    "revision": "ff0d31908206826ec137",
+    "url": "/poketrade/static/js/2.9ef032d4.chunk.js"
+  },
+  {
+    "revision": "ceaf96396243faf69ee4007e8b701525",
+    "url": "/poketrade/index.html"
+  }
+];
